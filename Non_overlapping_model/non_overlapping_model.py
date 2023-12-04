16edf4ee20eb067effbbdca4b3fb7c0d20b3bc35
UrakadakiRemuru <met-zi@mail.ru>
2023-12-04 15:50:37 +0300

    non_overlapping_border

--- Non_overlapping_model/non_overlapping_model.py
+++ Non_overlapping_model/non_overlapping_model.py
@@ -14,10 +14,12 @@ def transform(elp: ellipse, c: Optional[Tuple] = None) -> tuple:
     '''
     if c is None:
         return tuple(
-            np.linalg.multi_dot([inverse(sqrt_diagonal(P(elp.a, elp.b))), transpose(Q(elp.angle)), transpose(elp.coord_to_array())]))
+            np.linalg.multi_dot(
+                [inverse(sqrt_diagonal(P(elp.a, elp.b))), transpose(Q(elp.angle)), transpose(elp.coord_to_array())]))
     else:
         return tuple(
-            np.linalg.multi_dot([inverse(sqrt_diagonal(P(elp.a, elp.b))), transpose(Q(elp.angle)), transpose(np.array(list(c)))]))
+            np.linalg.multi_dot(
+                [inverse(sqrt_diagonal(P(elp.a, elp.b))), transpose(Q(elp.angle)), transpose(np.array(list(c)))]))
 
 
 def find_params_for_transformed_ellipse(S_ij: np.array, c: tuple) -> ellipse:
@@ -55,3 +57,24 @@ def transform_ellipse(elp_i: ellipse, elp_j: ellipse) -> ellipse:
     S_ij = S(P(elp_i.a, elp_i.b), Q(elp_i.angle), P(elp_j.a, elp_j.b), Q(elp_j.angle))
     print(S_ij)
     return find_params_for_transformed_ellipse(S_ij, c_j)
+
+
+def not_overlapping_border(elp: ellipse, width: float, height: float):
+    w_list = [np.array([1, 0]), -np.array([1, 0]), np.array([0, 1]), -np.array([0, 1])]
+    w_list = [transpose(w) for w in w_list]
+    s_list = [width / 2, -width / 2, height / 2, -height / 2]
+    for border in range(4):
+        if (np.dot(transpose(w_list[border]), elp.coord) - s_list[border]) ** 2 >= \
+                np.linalg.norm(np.linalg.multi_dot([sqrt_diagonal(P(elp.a, elp.b)),
+                                                    transpose(Q(elp.angle)),
+                                                    w_list[border]])) ** 2 and \
+                np.dot(transpose(w_list[border]), elp.coord) <= s_list[border]:
+            '''print((np.dot(transpose(w_list[border]), elp.coord) - s_list[border]) ** 2, \
+                np.linalg.norm(np.linalg.multi_dot([sqrt_diagonal(P(elp.a, elp.b)),
+                                                    transpose(Q(elp.angle)),
+                                                    w_list[border]])) ** 2)
+            print(np.dot(transpose(w_list[border]), elp.coord), s_list[border])'''
+            marker = True
+        else:
+            marker = False
+    return marker
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
-from matplotlib.patches import Ellipse, Circle
+from matplotlib.patches import Ellipse, Circle, Rectangle
 
 from Generation.generation import ellipse
-from Non_overlapping_model.non_overlapping_model import transform, transform_ellipse
+from Non_overlapping_model.non_overlapping_model import transform, transform_ellipse, not_overlapping_border
 
-# w = 10
-# h = 10
+w = 10
+h = 10
 # elps = generate_ellipse(2, w, h)
 # draw_scene(elps, w, h)
 
@@ -20,9 +20,14 @@ ax1.set(xlim=(-10, 10), ylim=(-10, 10), aspect="equal")
 ax2.set(xlim=(-10, 10), ylim=(-10, 10), aspect="equal")
 elp_i = Ellipse(elp1.coord, elp1.a, elp1.b, angle=elp1.angle)
 elp_j = Ellipse(elp2.coord, elp2.a, elp2.b, angle=elp2.angle)
+rect = Rectangle((-5, -5), w, h, fill=False)
+
+for elp in [elp1, elp2]:
+    print(not_overlapping_border(elp, w, h))
 
 ax1.add_artist(elp_i)
 ax1.add_artist(elp_j)
+ax1.add_artist(rect)
 
 eps_ii = Circle(c_r, 1)
 eps_ij = Ellipse(trans_elp.coord, trans_elp.a, trans_elp.b, angle=trans_elp.angle)
